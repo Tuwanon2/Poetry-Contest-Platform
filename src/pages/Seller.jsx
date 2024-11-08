b490ac83ddf557ecd70ef8b168798072a411b9dc
Tar <Kittiwiriya_c@silpakorn.edu>
2024-11-08 15:45:10 +0700

    Resolve merge conflicts

--- src/pages/Seller.jsx
+++ src/pages/Seller.jsx
@@ -4,7 +4,7 @@ import TopNav from '../components/TopNav';
 import TopMenu from '../components/TopMenu';
 import Footer from '../components/Footer';
 import Products from '../components/Products';
-import AllMarket from '../components/AllMarket';
+
 
 const Seller = () => {
   const { sellerId } = useParams(); // ดึง sellerId จาก URL
