d6bb916d506c04d79b47755a5fd850f9f6037e3c
Tar <Kittiwiriya_c@silpakorn.edu>
2024-11-13 01:24:16 +0700

    Resolve merge conflicts

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -145,16 +145,17 @@ const MyCart = () => {
                                             <strong>ราคา: ฿{product.price * (product.quantity || 1)}</strong>
                                         </Card.Text>
                                         <button
+                                            className="custom-button"
                                             onClick={() => removeProduct(product.id)}
-                                            style={{
-                                                color: 'white',
-                                                backgroundColor: 'red',
-                                                cursor: 'pointer',
-                                                fontSize: '0.9rem',
-                                            }}
-                                        >
-                                            นำสินค้าออก
+>
+                                            <span className="custom-text">Delete</span>
+                                            <span className="custom-icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                            <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                                            </svg>
+                                            </span>
                                         </button>
+
                                     </Card.Body>
                                 </Col>
                             </Row>
