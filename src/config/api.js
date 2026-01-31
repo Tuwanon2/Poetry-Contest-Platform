d8b955941458f532ff157908a89fdc15996b93a3
Your Name <thuwanon2547@gmail.com>
2026-02-01 00:57:27 +0700

    wadad

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 // src/config/api.js
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://poetry-contest-platform-production.up.railway.app/api/v1';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://poetry-contest-platform-production.up.railway.app/api/v1';
 export default API_BASE_URL;
