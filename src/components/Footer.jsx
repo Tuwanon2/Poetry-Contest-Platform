19f41daa5246b774d80f84e6df725c12622e47d9
Tuwanon2 <115211211+Tuwanon2@users.noreply.github.com>
2026-02-01 21:33:22 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className="footer-edu">
-    <span>web application นี้ จัดทำเพื่อการศึกษาเท่านั้น</span>
+    <span>Web application นี้ จัดทำเพื่อการศึกษาเท่านั้น</span>
   </footer>
 );
 
