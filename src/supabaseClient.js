6a6123c22ad81ea4d03d3e2289c38b8ec763d666
Your Name <thuwanon2547@gmail.com>
2026-02-01 18:07:21 +0700

    urlanon

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // TODO: Replace with your actual Supabase project credentials
 const SUPABASE_URL = 'https://ekpstgavxcnmmrhqswmc.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_QglE8ympLl-WeJBdFSqCBA_PS8o5i6d';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbnhkZXV5b2tqcnJ6bnJzbm9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NTYwNjUsImV4cCI6MjA4NTQzMjA2NX0.SbXuIqzI0WaiYmDxRE8RGOvKeTXor9Ex0CTYzm02i38';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
